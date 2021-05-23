3df706092d4e0ed8133b1ea9fe30dd2756a899cf
Dhanush Kumar <dhanush1926@gmail.com>
2021-05-23 17:24:02 +0530

    rafa-ui-6 // Added header

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,16 @@
 <template>
   <div id="app">
+    <Header />
   </div>
 </template>
+
+<script>
+import Header from "@/components/Header";
+
+export default {
+  name: "app",
+  components: {
+    Header,
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-app-bar color="#1B1A17" dense>
+      <v-toolbar-title>
+        <img height="100px" src="@/assets/logo/logo.png" />
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn class="orange">Sign Up</v-btn>
+    </v-app-bar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
