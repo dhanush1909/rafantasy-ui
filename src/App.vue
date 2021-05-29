462d300ea357a9742e639ac0c6f213f91c6f94d2
Dhanush Kumar <dhanush1926@gmail.com>
2021-05-29 20:10:54 +0530

    rafa-ui-1 and 4 // Added login page with login component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <Header />
+    <AuthCommon />
   </div>
 </template>
 
 <script>
 import Header from "@/components/Header";
+import AuthCommon from "@/views/authCommon/AuthCommon";
 
 export default {
   name: "app",
   components: {
     Header,
+    AuthCommon,
   },
 };
 </script>
---
+++ src/components/Login.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="login-main">
+    <v-form class="main" v-model="valid">
+      <v-row>
+        <v-col><span class="title">Sign In</span></v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="email"
+            :rules="emailRules"
+            label="E-mail"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col cols="12">
+          <v-text-field
+            v-model="password"
+            :rules="passwordRules"
+            type="password"
+            label="Password"
+            required
+          ></v-text-field>
+        </v-col>
+        <v-col><v-btn class="orange">Sign In</v-btn></v-col>
+      </v-row>
+    </v-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      email: "",
+      password: "",
+      emailRules: [
+        (v) => !!v || "E-mail is required",
+        (v) => /.+@.+/.test(v) || "E-mail must be valid",
+      ],
+      passwordRules: [(v) => !!v || "Password is required"], // TODO: Moves common rules to utils file
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+// $input-slot-margin-bottom: none; //TODO: figure out a way to overwrite vuetify sass variables
+
+.login-main {
+  width: 100%;
+  height: 100%;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .v-form {
+    width: 70%;
+
+    .col {
+      display: flex;
+      justify-content: center;
+
+      .title {
+        font-size: 24px;
+        color: #ffffff;
+      }
+    }
+
+    .v-input {
+      .v-label {
+        padding-left: 5px;
+        font-size: 0.8rem;
+      }
+
+      .v-label--active {
+        top: 0;
+        color: white;
+      }
+
+      .v-messages {
+        color: red;
+      }
+    }
+
+    .v-input__control > .v-input__slot:before,
+    .v-input__control > .v-input__slot:after {
+      border: none;
+      border-style: none;
+    }
+
+    .v-text-field input {
+      border: none;
+      background-color: white;
+      border-radius: 20px;
+      height: 30px;
+      padding: 0 10px;
+    }
+
+    .v-text-field input[type="text"] {
+      font-size: 14px;
+    }
+  }
+}
+</style>
