4c72bafbd11d513c97af6bd3cae93f3f1172f378
Acyuta <56015756+acyuta1@users.noreply.github.com>
2021-06-19 02:18:31 +0530

    added user-store, stored user info on login and removed on logout

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,28 +1,41 @@
-import Keycloak from 'keycloak-js';
+import Keycloak from "keycloak-js";
+import store from "../store";
 
 const INIT_OPTIONS = {
-  url: 'http://127.0.0.1:8180/auth', realm: 'Rafantasy', clientId: 'rafantasy-client', onLoad: 'login-required'
-}
+  url: "http://127.0.0.1:8180/auth",
+  realm: "Rafantasy",
+  clientId: "rafantasy-client",
+  onLoad: "login-required",
+};
 
 class AuthService {
-    constructor() {
-        this.kc = Keycloak(INIT_OPTIONS);
-    }
+  constructor() {
+    this.kc = Keycloak(INIT_OPTIONS);
+  }
 
-    init = (callback) => {
-        this.kc.init({ onLoad: INIT_OPTIONS.onLoad }).then((auth) => {
-            if (!auth) {
-                window.location.reload();
-            } else {
-                callback();
-            }
+  init = (callback) => {
+    this.kc
+      .init({ onLoad: INIT_OPTIONS.onLoad })
+      .then((auth) => {
+        if (!auth) {
+          window.location.reload();
+        } else {
+          store.dispatch("user/loginSuccessful", {
+            parsedToken: this.kc.tokenParsed,
+            token: this.kc.token,
+          });
+          callback();
+        }
+      })
+      .catch(() => {
+        console.log("Authenticated Failed");
+      });
+  };
 
-        }).catch(() => {
-            console.log("Authenticated Failed");
-        });
-    }
-
-    logout = () => this.kc.logout();
+  logout = () => {
+    this.kc.logout();
+    store.dispatch("user/logoutSuccessful");
+  };
 }
 
 export default new AuthService();
